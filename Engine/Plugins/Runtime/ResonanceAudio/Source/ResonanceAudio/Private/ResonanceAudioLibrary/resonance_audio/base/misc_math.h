b5020bd82c8b14a77e2d86ea3301643136ad620c
Ben Marsh <Ben.Marsh@epicgames.com>
2019-06-13 21:58:03 -0400

    Merging all ResonanceAudio changes to fix warnings.
    #rb none
    #rnx
    #jira
    
    [CL 6987227 by Ben Marsh in 4.23 branch]

--- Engine/Plugins/Runtime/ResonanceAudio/Source/ResonanceAudio/Private/ResonanceAudioLibrary/resonance_audio/base/misc_math.h
+++ Engine/Plugins/Runtime/ResonanceAudio/Source/ResonanceAudio/Private/ResonanceAudioLibrary/resonance_audio/base/misc_math.h
@@ -29,9 +29,7 @@ limitations under the License.
 #include <vector>
 
 #include "base/integral_types.h"
-THIRD_PARTY_INCLUDES_START
-#include "Eigen/Dense"
-THIRD_PARTY_INCLUDES_END
+#include "ResonanceEigenHelper.h"
 #include "base/constants_and_types.h"
 #include "base/logging.h"
 
