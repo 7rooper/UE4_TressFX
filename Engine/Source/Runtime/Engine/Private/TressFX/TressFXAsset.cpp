3254bc988beb7cc1382ded8a80eee82831daab92
Jacob Kostenick <kostenickj@gmail.com>
2019-07-23 02:26:42 -0700

    fix out of bounds issues with simulation by doubling bounds size.

--- Engine/Source/Runtime/Engine/Private/TressFX/TressFXAsset.cpp
+++ Engine/Source/Runtime/Engine/Private/TressFX/TressFXAsset.cpp
@@ -316,7 +316,15 @@ FBoxSphereBounds FTressFXRuntimeData::CalcBounds()
 	{
 		Box += FVector(Positions[i]);
 	}
-	return FBoxSphereBounds(Box);
+
+	//Double the bounds to account for simulation
+	FTransform Transform;
+	Transform.SetScale3D(FVector(2, 2, 2));
+	FQuat ZeroRot = FQuat(EForceInit::ForceInitToZero);
+	ZeroRot.Normalize();
+	Transform.SetRotation(ZeroRot);
+	Transform.SetTranslation(FVector(0, 0, 0));
+	return FBoxSphereBounds(Box.TransformBy(Transform));
 }
 
 TArray<FVector> FTressFXRuntimeData::GetRootPositions()
