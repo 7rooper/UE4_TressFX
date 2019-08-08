a4cbcd06dfa6a8e20ddcccefeb5520b5397e1100
Dave Belanger <Dave.Belanger@epicgames.com>
2019-08-08 11:11:01 -0400

    Type safe FRenderThreadUpdateContext #jira none #rb Matt.Kuhlenschmidt
    [CL 7881852 by Dave Belanger in 4.23 branch]

--- Engine/Source/Runtime/SlateCore/Public/Rendering/SlateRenderer.h
+++ Engine/Source/Runtime/SlateCore/Public/Rendering/SlateRenderer.h
@@ -9,9 +9,11 @@
 #include "Templates/RefCounting.h"
 
 class FRHITexture2D;
+class FRenderTarget;
 class FSlateDrawBuffer;
 class FSlateUpdatableTexture;
 class ILayoutCache;
+class ISlate3DRenderer;
 class ISlateAtlasProvider;
 class ISlateStyle;
 class SWindow;
@@ -30,8 +32,8 @@ struct FRenderThreadUpdateContext
 	float WorldTimeSeconds;
 	float DeltaTimeSeconds;
 	float RealTimeSeconds;
-	void* RenderTargetResource;
-	void* Renderer;
+	FRenderTarget* RenderTarget;
+	ISlate3DRenderer* Renderer;
 	bool bClearTarget;
 };
 
