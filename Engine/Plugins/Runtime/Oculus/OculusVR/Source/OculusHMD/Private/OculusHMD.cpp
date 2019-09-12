a2e3792471ba0afd86bffd3f816da907f486cf3e
steve smith <steve.smith@epicgames.com>
2019-09-12 14:14:03 -0400

    Workaround for the Oculus depth compositor bug requiring alpha - add a runtime-settable CVar to override the read-only version which is set from EnableStereo in the Oculus HMD code.
    #jira UE-80257
    #rb ryan.vance
    #robomerge ignore
    #lockdown nick.whiting
    
    [CL 8658414 by steve smith in 4.23 branch]

--- Engine/Plugins/Runtime/Oculus/OculusVR/Source/OculusHMD/Private/OculusHMD.cpp
+++ Engine/Plugins/Runtime/Oculus/OculusVR/Source/OculusHMD/Private/OculusHMD.cpp
@@ -2042,6 +2042,10 @@ namespace OculusHMD
 
 		if (!ovrp_GetInitialized())
 		{
+			// 4.23.1 workaround: enable the alpha-channel in the tonemapper.
+			static auto AlphaSupportOverrideCVar = IConsoleManager::Get().FindConsoleVariable(TEXT("r.PostProcessing.PropagateAlphaOverride"));
+			AlphaSupportOverrideCVar->Set(1);
+
 #if !UE_BUILD_SHIPPING
 			ovrpLogCallback logCallback = OvrpLogCallback;
 #else
